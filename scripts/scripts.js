fbfba9287fbbb3331354d0ca7ae306c5642c4c62
Eskerepvp <75771072+Eskerepvp@users.noreply.github.com>
2022-03-25 22:53:50 -0700

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,27 @@
+
+document.getElementById("gamesButton").addEventListener("click", injectGames);
+
+document.getElementById("bypassButton").addEventListener("click", injectBypass);
+
+document.getElementById("consoleButton").addEventListener("click", injectConsole);
+
+
+// button functions
+
 function injectGames() {
-  document.body.innerHTML='<body><script src="https://github.com/Eskerepvp/DoorDash/blob/main/scripts/scripts.js"></script><style>.button1 {background-color:#161923; color: black; border: 2px solid #161923; }body {background-color:#161923; color:white; font-family: "Lucida Console", "Courier New", monospace;} .button1:hover {background-color: #D3D3D3;} .gamesDiv {text-align: center;}  <div class="gamesDiv"><button class="gameButtons tetrisButton" onclick="injectTetris()"><img src="https://github.com/Eskerepvp/DoorDash/blob/main/global-assets/tetris.png" height="200" width="200"</button></div></style><div class="bbaDiv"><button class="button button1" onclick="goBack()"><img src="https://github.com/Eskerepvp/DoorDash/blob/main/global-assets/backArrow.png" height="16" width="16"/></button></div></body>'
+  document.body.innerHTML = '<body> <script src="https://bitbucket.org/eskerepvp/doordash/raw/3f1167412bad9aad969bbdacee509fcf74815f0e/scripts/scripts.js"></script> <style> body {  background-color:#161923; font-family: "Lucida Console", "Courier New", monospace; } .gamesDiv { text-align: center; } </style> <div class="bbaDiv"><button class="button button1" onclick="goBack()"><img src="https://bitbucket.org/eskerepvp/doordash/raw/7c1735f5c0e3815194695a695b92f1079fcda294/global-assets/backArrow.png" height="16" width="16"/></button></div> <div class="gamesDiv"> <button class="gameButtons tetrisButton" onclick="injectTetris()"><img src="https://bitbucket.org/eskerepvp/doordash/raw/7c1735f5c0e3815194695a695b92f1079fcda294/global-assets/tetris.png" height="200" width="200"/></button> </div> </body> '
 }
 
-function goBack() {
-  window.location.reload();
-  (function(){document.body.appendChild(document.createElement('script')).src='https://raw.githubusercontent.com/Eskerepvp/DoorDash/main/scripts/injectSite.js'})()
+function injectBypass() {
+  // ye so I wanna find a way to crash the teacher monitoring and like make my own hyper tabs thing but not with iframe
+  alert("Coming Soon™")
 }
 
 function injectConsole() {
   var firebug=document.createElement('script');firebug.setAttribute('src','https://luphoria.com/fbl/fbl/firebug-lite-debug.js');document.body.appendChild(firebug);(function(){if(window.firebug.version){firebug.init();}else{setTimeout(arguments.callee);}})();void(firebug);
 }
 
-function injectBypass() {
-
+function goBack() {
+  window.location.reload();
+  (function(){document.body.appendChild(document.createElement('script')).src='https://bitbucket.org/eskerepvp/doordash/raw/858ffbf09fec3e1b1d05bf7c80f15ea0b6c091bc/scripts/injectSite.js'})()
 }
